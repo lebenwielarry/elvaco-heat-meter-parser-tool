21f2ce2f393641da33cc0100fa12b79885c745f1
Annalena Frey <annalena-frey1@web.de>
2025-02-12 19:58:10 +0100

    krise hier

--- ui/parser/uh30_annalena/uh30.js
+++ ui/parser/uh30_annalena/uh30.js
@@ -1117,7 +1117,7 @@ function parseMonitoringMessage(payload) {
 }
 
 
-export function parserForUH30(payload) {
+function parserForUH30(payload) {
   const messageFormat = payload.substr(0, 2)
 
   switch (messageFormat) {
@@ -1177,7 +1177,7 @@ const meter_communication_error_payload = "053406FFFFFFFF3414FFFFFFFF322DFFFF323
 //meter_comm_error_test = parserForUH30(meter_communication_error_payload)
 //console.log(meter_comm_error_test)
 
-/* module.exports = {
+export {
   parserForUH30,
   scaleEnergy,
   scaleFlow,
@@ -1192,5 +1192,5 @@ const meter_communication_error_payload = "053406FFFFFFFF3414FFFFFFFF322DFFFF323
   scalePreviousMonthEnergy,
   analyseError,
   checkMeterCommunicationError
-}; */
+};
 
